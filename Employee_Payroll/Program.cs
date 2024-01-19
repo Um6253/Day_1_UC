2a096cd351431122e8233624a5e039bccfb16c6f
Um6253 <um6253@srmist.edu.in>
2024-01-19 17:01:12 +0530

    [Manikanta] Add: Adding the Use Case 3 Program

--- Employee_Payroll/Program.cs
+++ Employee_Payroll/Program.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uc2_code
+namespace uc3_code
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int wageRate = 20;
-            int dayHrs = 8;
-            Console.WriteLine("Your daily wage is : " + (Convert.ToInt32(wageRate * dayHrs)));
-            Console.ReadLine();
+            Console.WriteLine("Enter your role either Fulltime or Partime");
+            String role = Console.ReadLine();
 
+            if (role == "Fulltime")
+            {
+                Console.WriteLine($"Your daily wage as {role} is : " + 160);
+            }
+            else
+            {
+                Console.WriteLine($"Your daily wage as {role} is : " + 80);
+            }
+            Console.ReadLine();
         }
     }
 }
